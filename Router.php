641fd1c2a37cfb24d8a44c380376347277f0e7fe
Roberto <rgalanllorca@me.com>
2023-03-06 00:16:55 +0100

    Arreglando funcion comprobarRutas()

--- Router.php
+++ Router.php
@@ -20,7 +20,7 @@ class Router
     public function comprobarRutas()
     {
 
-        $url_actual = $_SERVER['PATH_INFO'] ?? '/';
+        $url_actual = strtok($_SERVER['REQUEST_URI'], '?') ?? '/';
         $method = $_SERVER['REQUEST_METHOD'];
 
         if ($method === 'GET') {
@@ -49,7 +49,7 @@ class Router
         $contenido = ob_get_clean(); // Limpia el Buffer
 
         // Utilizar el layout de acuerdo a la URL
-        $url_actual = $_SERVER['PATH_INFO'] ?? '/';
+        $url_actual = strtok($_SERVER['REQUEST_URI'], '?') ?? '/';
 
         if(str_contains($url_actual, 'admin')) {
             include_once __DIR__ . '/views/admin-layout.php';
